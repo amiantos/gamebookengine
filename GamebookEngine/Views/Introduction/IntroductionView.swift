818f80834ae0e3b24a15f5ebbd5aabbb7dd95228
Brad Root <bradroot@me.com>
2023-04-05 15:58:53 -0700

    add icons and get card content somewhat into place

--- GamebookEngine/Views/Introduction/IntroductionView.swift
+++ GamebookEngine/Views/Introduction/IntroductionView.swift
@@ -6,50 +6,83 @@
 //  Copyright © 2023 Brad Root. All rights reserved.
 //
 
-import SwiftUI
 import CardStack
+import SwiftUI
+
+struct CardData: Identifiable {
+    var id = UUID()
+    let title: String
+    let text: String
+    let image: Image
+}
 
 struct CardView: View {
-    let text: String
+    let data: CardData
 
     var body: some View {
         VStack {
-                Text("Card")
-                HStack {
-                    Text(self.text)
-                  Spacer()
-                    Text("\(self.text) km away")
-                    .font(.footnote)
-                    .foregroundColor(.secondary)
-                }
+            Text(self.data.title)
+                .font(.title)
                 .padding()
-              }
-              .background(Color.white)
-              .cornerRadius(12)
-              .shadow(radius: 4)
+            Spacer()
+            GeometryReader { geo in
+                self.data.image
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: geo.size.width * 0.8)
+                    .frame(width: geo.size.width, height: geo.size.height)
             }
+            Spacer()
+            Text(self.data.text)
+                .minimumScaleFactor(0.5)
+                .padding()
+        }
+        .background(Color.white)
+        .cornerRadius(10)
+        .shadow(radius: 4)
     }
+}
 
 struct IntroductionView: View {
-    @State var cards: [String] = ["Foo", "Bar", "Foo"]
-    
+    @State var cards: [CardData] = [
+        CardData(
+            title: "Gamebook Engine", text: "Welcome to Gamebook Engine, the all-in-one app for creating "
+                + "and playing interactive stories, entirely on your phone or "
+                + "tablet. No account needed, no cloud services, total privacy.", image: Image("welcome-icon")
+        ),
+        CardData(
+            title: "Create", text: "Edit gamebooks or create your own! Gamebook Engine has"
+                + "a fully-featured interface for creating full length choosable path adventure "
+                + "games where readers get to influence the story.", image: Image("create-icon")
+        ),
+        CardData(
+            title: "Play", text: "And of course, you can play gamebooks! Gamebook Engine comes "
+                + "with a few simple games built in that you can play, but it’s more fun to play "
+                + "games made by friends and family.", image: Image("play-icon")
+        ),
+        CardData(
+            title: "Share", text: "Stories written with Gamebook Engine can be exported to a "
+                + "plain text (JSON formatted) file that you can send to your friends. To play, they "
+                + "just need their own copy of Gamebook Engine.", image: Image("share-icon")
+        ),
+    ]
+
     var body: some View {
         CardStack(
-              direction: LeftRight.direction,
-              data: cards,
-              onSwipe: { text, direction in
+            direction: LeftRight.direction,
+            data: cards,
+            onSwipe: { text, direction in
                 print("Swiped \(text) to \(direction)")
-              },
-              content: { text, _, _ in
-                CardView(text: text)
-              }
-            )
-            .padding()
-            .scaledToFit()
-            .frame(alignment: .center)
-            .navigationBarTitle("Basic", displayMode: .inline)
-          }
+            },
+            content: { data, _, _ in
+                CardView(data: data)
+            }
+        )
+        .padding()
+        .scaledToFit()
+        .frame(alignment: .center)
     }
+}
 
 struct IntroductionView_Previews: PreviewProvider {
     static var previews: some View {
